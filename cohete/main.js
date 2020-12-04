5165c3c0b4025f85f8d1493ea63bf509e5aced7f
mario-sanmartin <sanmartin.mario@outlook.com>
2020-12-04 01:04:10 -0300

    Cris Morris volador

--- cohete/main.js
+++ cohete/main.js
@@ -1,9 +1,31 @@
+document.addEventListener('mousemove',function(e){
+    const cris = document.querySelector('.cris');
+
+    cris.style.left = e.offsetX + 'px';
+    cris.style.top = e.offsetY + 'px';
+})
+
+
+
 function estrellas(){
     let contador = 50;
     let escena = document.querySelector('.escena');
     let i = 0;
 
     while(i < contador){
-        const estrela = document.createElement('i');
+        let estrella = document.createElement('i');
+        let x = Math.floor(Math.random() * window.innerWidth);
+        
+        let duracion = Math.random() * 1;
+        let h = Math.random() * 100;
+        
+        estrella.style.left = x + 'px';
+        estrella.style.width = 1 + 'px';
+        estrella.style.height = 50 + h + 'px';
+        estrella.style.animationDuration = duracion + 's';
+        
+        escena.appendChild(estrella);
+        i++
     }
 }
+estrellas();
