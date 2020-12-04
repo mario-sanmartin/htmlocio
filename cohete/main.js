bf6df103dc9d09680ce0032991bd6c28d6e007cd
mario-sanmartin <sanmartin.mario@outlook.com>
2020-12-04 00:05:48 -0300

    Cris parkinson

---
+++ cohete/main.js
@@ -0,0 +1,9 @@
+function estrellas(){
+    let contador = 50;
+    let escena = document.querySelector('.escena');
+    let i = 0;
+
+    while(i < contador){
+        const estrela = document.createElement('i');
+    }
+}
